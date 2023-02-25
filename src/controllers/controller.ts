07430d90f53ebc2bad661da55b2503db3f6d58d7
nagykrisztian <krisznagy2001@gmail.com>
2023-02-25 17:09:08 +0100

    ♻️Refactor to follow rules

--- src/controllers/controller.ts
+++ src/controllers/controller.ts
@@ -1,18 +1,13 @@
-import { Request, Response } from "express";
-import { MysqlError } from "mysql";
-import mssql from "../sql";
+import { Request, Response } from 'express';
+import mssql from '../sql';
 
 export default class Controller {
   public testMethod = (req: Request, res: Response) => {
-
     mssql.query('select 1+1 as result', function (err: any, recordset: any) {
-
-      if (err) console.log(err)
+      if (err) console.log(err);
 
       // send records as a response
       res.send(recordset);
-
     });
-
-  }
+  };
 }
