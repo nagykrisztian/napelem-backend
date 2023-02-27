817b36539eb05b233c28e390290cc1dec8b1a970
nagykrisztian <krisznagy2001@gmail.com>
2023-02-27 20:00:18 +0100

    🚧CORS policy tests

--- src/app.ts
+++ src/app.ts
@@ -18,18 +18,19 @@ class App {
   private expressConfig(): void {
     this.app.use(bodyParser.json());
 
-    // // eslint-disable-next-line consistent-return
-    // this.app.use((req, res, next) => {
-    //   res.header('Access-Control-Allow-Origin', '*');
+    // eslint-disable-next-line consistent-return
+    this.app.use((req, res, next) => {
+      res.header('Access-Control-Allow-Origin', '*');
 
-    //   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    //   res.header('Set-Cookie', 'HttpOnly;Secure;SameSite=None');
-    //   if (req.method === 'OPTIONS') {
-    //     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    //     return res.status(200).json({});
-    //   }
-    //   next();
-    // });
+      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+      res.header('Set-Cookie', 'HttpOnly;Secure;SameSite=None');
+      if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+        return res.status(200).json({});
+      }
+
+      next();
+    });
     this.app.use(bodyParser.urlencoded({ extended: false }));
   }
 }
