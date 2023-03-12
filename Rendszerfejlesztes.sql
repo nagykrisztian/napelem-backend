4769036f8d365bc48ad8f70c44daf0ef60c81747
nagykrisztian <krisznagy2001@gmail.com>
2023-03-12 18:49:38 +0100

    🗃️Modifies structure of storage table
    adds new attrib to check availability
    changes primary key

--- Rendszerfejlesztes.sql
+++ Rendszerfejlesztes.sql
@@ -1,5 +1,20 @@
+GO
+
+DROP TABLE IF EXISTS Telemetry
+DROP TABLE IF EXISTS Parts_needed
+DROP TABLE IF EXISTS Storage
+DROP TABLE IF EXISTS Projects
+DROP TABLE IF EXISTS Customers
+DROP TABLE IF EXISTS [States]
+DROP TABLE IF EXISTS Users
+DROP TABLE IF EXISTS [Permissions]
 DROP TABLE IF EXISTS Parts
-CREATE TABLE Parts(
+
+GO
+
+DROP TABLE IF EXISTS Parts
+CREATE TABLE Parts
+(
     partID INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
     partName VARCHAR(50),
     price INT,
@@ -7,26 +22,30 @@ CREATE TABLE Parts(
 )
 
 DROP TABLE IF EXISTS [Permissions]
-CREATE TABLE [Permissions](
+CREATE TABLE [Permissions]
+(
     permissionID INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
     permissionName VARCHAR(50)
 )
 
 DROP TABLE IF EXISTS Users
-CREATE TABLE Users(
+CREATE TABLE Users
+(
     username VARCHAR(50) NOT NULL PRIMARY KEY,
     [password] VARCHAR(100) NOT NULL,
-    perrmissionID INT NOT NULL FOREIGN KEY REFERENCES [Permissions](permissionID) ON UPDATE cascade
+    perrmissionID INT NOT NULL FOREIGN KEY REFERENCES [Permissions](permissionID) ON UPDATE CASCADE
 )
 
 DROP TABLE IF EXISTS [States]
-CREATE TABLE [States](
+CREATE TABLE [States]
+(
     stateID INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
     stateName VARCHAR(50),
 )
 
 DROP TABLE IF EXISTS Customers
-CREATE TABLE Customers(
+CREATE TABLE Customers
+(
     SSN VARCHAR(50) NOT NULL PRIMARY KEY,
     [name] VARCHAR(50),
     [address] VARCHAR(50),
@@ -35,61 +54,57 @@ CREATE TABLE Customers(
 )
 
 DROP TABLE IF EXISTS Projects
-CREATE TABLE Projects(
+CREATE TABLE Projects
+(
     projectID INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
-    username VARCHAR(50) NOT NULL FOREIGN KEY REFERENCES Users(username) ON UPDATE cascade,
+    username VARCHAR(50) NOT NULL FOREIGN KEY REFERENCES Users(username) ON UPDATE CASCADE,
     orderDate DATE,
     workingTime INT,
     laborFee INT,
     [address] VARCHAR(50),
     [description] VARCHAR(50),
-    customerSSN VARCHAR(50) FOREIGN KEY REFERENCES Customers(SSN) ON UPDATE cascade ON DELETE SET NULL,
-    currentStateID INT FOREIGN KEY REFERENCES [States](stateID)ON UPDATE cascade ON DELETE SET NULL
+    customerSSN VARCHAR(50) FOREIGN KEY REFERENCES Customers(SSN) ON UPDATE CASCADE ON DELETE SET NULL,
+    currentStateID INT FOREIGN KEY REFERENCES [States](stateID)ON UPDATE CASCADE ON DELETE SET NULL
 )
 
 DROP TABLE IF EXISTS Storage
-CREATE TABLE Storage(
-    ID INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+CREATE TABLE Storage
+(
     [row] INT NOT NULL,
     [column] INT NOT NULL,
     [level] INT NOT NULL,
-    partID INT FOREIGN KEY REFERENCES Parts(partID) ON UPDATE cascade ON DELETE SET NULL,
-    currentPieces INT
+    partID INT FOREIGN KEY REFERENCES Parts(partID) ON UPDATE CASCADE ON DELETE SET NULL,
+    currentPieces INT,
+    available BIT NOT NULL,
+    -- 0 or 1 to represent boolean value
+    PRIMARY KEY([row],[column],[level])
 )
 
 DROP TABLE IF EXISTS Parts_needed
-CREATE TABLE Parts_needed(
-    partID INT NOT NULL FOREIGN KEY REFERENCES Parts(partID) ON UPDATE cascade,
-    projectID INT NOT NULL FOREIGN KEY REFERENCES Projects(projectID) ON UPDATE cascade,
+CREATE TABLE Parts_needed
+(
+    partID INT NOT NULL FOREIGN KEY REFERENCES Parts(partID) ON UPDATE CASCADE,
+    projectID INT NOT NULL FOREIGN KEY REFERENCES Projects(projectID) ON UPDATE CASCADE,
     reguiredQuantity INT,
     PRIMARY KEY(partID, projectID)
 )
 
 DROP TABLE IF EXISTS Telemetry
-CREATE TABLE Telemetry(
+CREATE TABLE Telemetry
+(
     projectID INT NOT NULL FOREIGN KEY REFERENCES Projects(projectID),
     stateID INT NOT NULL FOREIGN KEY REFERENCES [States](stateID),
     startDate DATE,
     finishDate DATE,
     PRIMARY KEY(projectID, stateID)
 )
--- GO
--- DROP TABLE IF EXISTS Telemetry
--- DROP TABLE IF EXISTS Parts_needed
--- DROP TABLE IF EXISTS Storage
--- DROP TABLE IF EXISTS Projects
--- DROP TABLE IF EXISTS Customers
--- DROP TABLE IF EXISTS [States]
--- DROP TABLE IF EXISTS Users
--- DROP TABLE IF EXISTS [Permissions]
--- DROP TABLE IF EXISTS Parts
--- GO
 
+GO
 
-
-INSERT INTO Parts([partName], price, partPerBox)
-    VALUES
-    ('BSD Panel', 89990, 5), 
+INSERT INTO Parts
+    ([partName], price, partPerBox)
+VALUES
+    ('BSD Panel', 89990, 5),
     ('Huaewi Panel', 119000, 8),
     ('Huawei Inverter', 310000, 2),
     ('Fronius Inverter', 423000, 2),
@@ -97,41 +112,57 @@ INSERT INTO Parts([partName], price, partPerBox)
     ('Kábel 6mm', 1200, 200),
     ('Kábel 3mm', 800, 300)
 
-INSERT INTO [Permissions](permissionName)
-    VALUES('Szakember'),('Raktaros'),('Raktarvezeto')
+INSERT INTO [Permissions]
+    (permissionName)
+VALUES('Szakember'),
+    ('Raktaros'),
+    ('Raktarvezeto')
 
 INSERT INTO Users
-    VALUES
-    ('szakember1','58ac22875aee49f8f6ec08c5c7bdc20094fa0bf7384bf841d4ef6d8a6013a59c',1),-- szakember1, szakemberjelszo
-    ('raktaros1','4c28b8b24fed825f74f0df5749274682c7d88f3c08d816d52efe9c9a25e440d3',2),-- raktaros1, raktarosjelszo
-    ('raktarvezeto1','4bd05fb1bef4fafabce6dcd47491614d5bc9a089bf1c28cf7369ac5d3a313bee',3)-- raktarosvezeto1, raktarvezetojelszo
+VALUES
+    ('szakember1', '58ac22875aee49f8f6ec08c5c7bdc20094fa0bf7384bf841d4ef6d8a6013a59c', 1),
+    -- szakember1, szakemberjelszo
+    ('raktaros1', '4c28b8b24fed825f74f0df5749274682c7d88f3c08d816d52efe9c9a25e440d3', 2),
+    -- raktaros1, raktarosjelszo
+    ('raktarvezeto1', '4bd05fb1bef4fafabce6dcd47491614d5bc9a089bf1c28cf7369ac5d3a313bee', 3)
+-- raktarosvezeto1, raktarvezetojelszo
 
 INSERT INTO States
-    VALUES
-    ('New'),('Draft'),('Wait'),('Scheduled'),('InProgress'),('Completed'),('Failed')
+VALUES
+    ('New'),
+    ('Draft'),
+    ('Wait'),
+    ('Scheduled'),
+    ('InProgress'),
+    ('Completed'),
+    ('Failed')
 
 INSERT INTO Customers
-    VALUES
-    ('123456AB','Kovács Elek', 'Veszprém', '+36301234567','kovacs.elek@gmail.com'),
-    ('634792HF','Nagy Géza', 'Budapest', '+36201249857','nagy.geza@gmail.com'),
-    ('7654321KD','Kiss Etelka', 'Ajka', '+36706352040','kiss.etelka@gmail.com')
+VALUES
+    ('123456AB', 'Kovács Elek', 'Veszprém', '+36301234567', 'kovacs.elek@gmail.com'),
+    ('634792HF', 'Nagy Géza', 'Budapest', '+36201249857', 'nagy.geza@gmail.com'),
+    ('7654321KD', 'Kiss Etelka', 'Ajka', '+36706352040', 'kiss.etelka@gmail.com')
 
-INSERT INTO Projects(username, orderDate, workingTime, laborFee, [address], [description], customerSSN, currentStateID)
-    VALUES
-    ('szakember1', '2023-01-15',3, 200000,'Balatonalmádi','Kis nyaraló, nagy pénz', '123456AB', 1),
-    ('szakember1', '2023-02-27', 4, 100000,'Budapest','Nagy nyaraló, kis pénz', '634792HF', 1)
+INSERT INTO Projects
+    (username, orderDate, workingTime, laborFee, [address], [description], customerSSN, currentStateID)
+VALUES
+    ('szakember1', '2023-01-15', 3, 200000, 'Balatonalmádi', 'Kis nyaraló, nagy pénz', '123456AB', 1),
+    ('szakember1', '2023-02-27', 4, 100000, 'Budapest', 'Nagy nyaraló, kis pénz', '634792HF', 1)
 
 INSERT INTO Storage
-    VALUES
-    (1,1,1,1,5),
-    (1,2,1,2,8),
-    (1,3,1,3,2),
-    (2,1,1,4,2),
-    (2,2,1,5,10),
-    (2,3,1,6,200),
-    (3,1,1,7,300)
+VALUES
+    (1, 1, 1, 1, 5, 0),
+    (1, 2, 1, 2, 8, 0),
+    (1, 3, 1, 3, 2, 0),
+    (2, 1, 1, 4, 2, 0),
+    (2, 2, 1, 5, 10, 0),
+    (2, 3, 1, 6, 200, 0),
+    (3, 1, 1, 7, 300, 0)
 
-INSERT INTO Telemetry(projectID, stateID, startDate)
-    VALUES
-    (1,1,'2023-01-15'),
-    (2,1,'2023-02-27')
+INSERT INTO Telemetry
+    (projectID, stateID, startDate)
+VALUES
+    (1, 1, '2023-01-15'),
+    (2, 1, '2023-02-27')
+
+GO
